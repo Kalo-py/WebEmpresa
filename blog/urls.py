1f8a15af268f41d52b3896f471fd779f1d96c185
Carlos@DESKTOP-CTD8JUA <carlosservian09@gmail.com>
2023-12-17 17:06:48 -0300

    cambio 17 dic 2023

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     # Paths del core
 
     path("", views.blog, name="blog"),
+    #tomara los parametros que se toman en modificar blog para pasar la funcion de views llamada "category"
+    #como los parentesis <> es considerado una cadena se tiene que declarar que es un entero
+    path('category/<int:category_id>/',views.category,name="category")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404 #este sirve para los errores de pagina que no existen
+from .models import Post, Category
 # Create your views here.
 def blog(request):
     posts = Post.objects.all()
     return render(request,"blog/blog.html", {'posts':posts})
+
+def category(request, category_id):
+    # Obtiene la categoría específica utilizando el ID proporcionado en la URL
+    #utilizamos el get 404 para que pueda mostrar en caso de que se toma mal un id
+    category= get_object_or_404(Category,id=category_id)
+    posts= Post.objects.filter(categories=category)
+    # Renderiza la plantilla "blog/category.html" y pasa la categoría como contexto
+    return render(request,"blog/category.html",{'category':category,
+                                                    'posts':posts})
+    # En resumen, la línea de código en cuestión se encarga de recuperar
+    # todas las publicaciones que están asociadas a una categoría específica,
+    # utilizando la relación establecida en el modelo Post.
